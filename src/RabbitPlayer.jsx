98ebd4a2c44f7e1e7747588ad06951d5c97bb29a
adolhunter <mingshiwang123@gmail.com>
2020-06-14 16:24:44 -0500

    Integrate lyricparser using props

--- src/RabbitPlayer.jsx
+++ src/RabbitPlayer.jsx
@@ -1,38 +1,44 @@
 import React from "react";
-import parseLyrics from "./LyricParser.jsx";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import PropTypes from "prop-types";
+import { Lrc } from "lrc-kit";
 
-export default function RabbitPlayer() {
-  return (
-    <>
-      <Container>
-        <Row>
-          <Col xs="12">
-            {" "}
-            <div
-              className="rabbit-lyrics"
-              data-media="#audio-1"
-              data-height="700"
-              data-alignment="center"
-            >
-              {parseLyrics()}
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <audio id="audio-1" controls position="center" xs="12">
-              {/* <source src="audio-1.ogg" type="audio/ogg"></source> */}
-              <source
-                src="http://sd.sycdn.kuwo.cn/5cb26c647edbd0a3e1980ec8fc2f9225/5ee67f31/resource/n1/92/63/258260127.mp3"
-                type="audio/mpeg"
-              ></source>
-            </audio>
-          </Col>
-        </Row>
-      </Container>
-    </>
-  );
+export default class RabbitPlayer extends React.Component {
+  render() {
+    const lrc = Lrc.parse(this.props.lyrics);
+    return (
+      <>
+        <Container>
+          <Row>
+            <Col xs="12">
+              {" "}
+              <div
+                className="rabbit-lyrics"
+                data-media="#audio-1"
+                data-height="700"
+                data-alignment="center"
+              >
+                {lrc.toString({ combine: false })}
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <audio id="audio-1" controls position="center" xs="12">
+                {/* <source src="audio-1.ogg" type="audio/ogg"></source> */}
+                <source src={this.props.music} type="audio/mpeg"></source>
+              </audio>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
 }
+
+RabbitPlayer.propTypes = {
+  music: PropTypes.string,
+  lyrics: PropTypes.string,
+};
