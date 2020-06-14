0d6d686810621a4767c6533050f3c4f53a72ad9c
adolhunter <mingshiwang123@gmail.com>
2020-06-14 15:13:31 -0500

    remove unused components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import Player from "./AudioPlayer";
-// import parseLyrics from './LyricParser.jsx';
 // eslint-disable-next-line no-unused-vars
 import RabbitLyrics from "rabbit-lyrics";
 import NavBar from './NavBar.jsx';
@@ -15,7 +13,6 @@ class MusicPlayer extends React.Component {
       <>
         <NavBar />
         <RabbitPlayer />
-        {/* <h1>{parseLyrics()}</h1> */}
       </>
     );
   }
--- src/RabbitPlayer.jsx
+++ src/RabbitPlayer.jsx
@@ -1,24 +1,38 @@
 import React from "react";
-import parseLyrics from './LyricParser.jsx';
+import parseLyrics from "./LyricParser.jsx";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function RabbitPlayer() {
   return (
     <>
-      <div
-        className="rabbit-lyrics"
-        data-media="#audio-1"
-        data-height="500"
-        data-alignment="center"
-      >
-        {parseLyrics()}
-      </div>
-      <audio id="audio-1" controls>
-        {/* <source src="audio-1.ogg" type="audio/ogg"></source> */}
-        <source
-          src="http://sd.sycdn.kuwo.cn/5cb26c647edbd0a3e1980ec8fc2f9225/5ee67f31/resource/n1/92/63/258260127.mp3"
-          type="audio/mpeg"
-        ></source>
-      </audio>
+      <Container>
+        <Row>
+          <Col>
+            {" "}
+            <div
+              className="rabbit-lyrics"
+              data-media="#audio-1"
+              data-height="500"
+              data-alignment="center"
+            >
+              {parseLyrics()}
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <audio id="audio-1" controls>
+              {/* <source src="audio-1.ogg" type="audio/ogg"></source> */}
+              <source
+                src="http://sd.sycdn.kuwo.cn/5cb26c647edbd0a3e1980ec8fc2f9225/5ee67f31/resource/n1/92/63/258260127.mp3"
+                type="audio/mpeg"
+              ></source>
+            </audio>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
