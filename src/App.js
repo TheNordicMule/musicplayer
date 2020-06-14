f5caa32397eeb91db1f5d90d97285b773bdcd4eb
adolhunter <mingshiwang123@gmail.com>
2020-06-14 00:46:41 -0500

    add bootstrap component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
 import "./App.css";
-import Player from './AudioPlayer';
+import Player from "./AudioPlayer";
+import Navbar from "react-bootstrap/Navbar";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class MusicPlayer extends React.Component {
-    render() {
-      return(
+  render() {
+    return (
+      <>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="/logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            LAXIS
+          </Navbar.Brand>
+        </Navbar>
         <Player />
-      );
-    }
+      </>
+    );
   }
+}
 
 export default MusicPlayer;
