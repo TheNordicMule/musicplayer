a66e092b6680bbd81624d49f3d81210ce0364268
adolhunter <mingshiwang123@gmail.com>
2020-06-16 15:11:02 -0500

    finish scrolling script implementation

--- src/RabbitPlayer.jsx
+++ src/RabbitPlayer.jsx
@@ -3,10 +3,14 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import PropTypes from "prop-types";
-// eslint-disable-next-line no-unused-vars
 import RabbitLyrics from "rabbit-lyrics";
 
 export default class RabbitPlayer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.mediaRef = React.createRef();
+    this.textRef = React.createRef();
+  }
   render() {
     return (
       <>
@@ -15,10 +19,10 @@ export default class RabbitPlayer extends React.Component {
             <Col xs="12">
               {" "}
               <div
-                className="rabbit-lyrics"
-                data-media="#audio-1"
+                data-media="lyrics-1"
                 data-height="700"
                 data-alignment="center"
+                ref={this.textRef}
               >
                 {this.props.lyrics}
               </div>
@@ -26,7 +30,13 @@ export default class RabbitPlayer extends React.Component {
           </Row>
           <Row>
             <Col>
-              <audio id="audio-1" controls position="center" xs="12">
+              <audio
+                id="audio-1"
+                controls
+                position="center"
+                xs="12"
+                ref={this.mediaRef}
+              >
                 <source src={this.props.music} type="audio/mpeg"></source>
               </audio>
             </Col>
@@ -37,15 +47,33 @@ export default class RabbitPlayer extends React.Component {
   }
 
   async componentDidUpdate() {
-      const audio = document.getElementById("audio-1");
-      try {
-        await audio.pause();
-        await audio.load();
-        await audio.play();
-      } catch (e) {
-        console.log("The user interrupted the playback");
-      }
+    const audio = document.getElementById("audio-1");
+    try {
+      await audio.pause();
+      await audio.load();
+      await audio.play();
+    } catch (e) {
+      console.log("The user interrupted the playback");
     }
+
+    this.textRef.current.classList.remove("rabbit-lyrics");
+    this.textRef.current.classList.remove("rabbit-lyrics--default");
+    this.textRef.current.classList.remove("rabbit-lyrics--enabled");
+    this.textRef.current.classList.remove("rabbit-lyrics--playing");
+    new RabbitLyrics({
+      element: this.textRef.current,
+      mediaElement: this.mediaRef.current,
+      height: 800,
+    });
+  }
+
+  componentDidMount() {
+    new RabbitLyrics({
+      element: this.textRef.current,
+      mediaElement: this.mediaRef.current,
+      height: 800,
+    });
+  }
 }
 
 RabbitPlayer.propTypes = {
