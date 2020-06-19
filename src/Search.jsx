7ae21db7fe55114cdffc31e0a8da86789d627713
adolhunter <mingshiwang123@gmail.com>
2020-06-19 01:23:27 -0500

    implement search

--- src/Search.jsx
+++ src/Search.jsx
@@ -30,7 +30,7 @@ export default function Search() {
             Highlight
           </Button>
           <Button variant="dark" onClick={() => changeLyricsProperty("revert")}>
-            Revert highlights
+            Revert All
           </Button>
         </Col>
       </Form.Row>
@@ -39,20 +39,47 @@ export default function Search() {
   return form;
 
   function changeLyricsProperty(option) {
-    const text = input.current.value;
+    if (option === "search") {
+      revertSearch();
+    }
+    let text = input.current.value;
+    if (option === "revert") {
+      revertSearch();
+      revertHighlighted();
+    }
     const divs = document.getElementsByClassName("rabbit-lyrics__line");
     const length = divs.length;
     for (let i = 0; i < length; i++) {
       const div = divs.item(i);
-      div.classList.remove("rabbit-lyrics-highlighted");
-      div.classList.remove("rabbit-lyrics-searched");
-      const lyric = div.innerText;
-      if (option === "search" && lyric.includes(text)) {
-        div.classList.add("rabbit-lyrics-searched");
+      if (option === "search" && div.innerHTML.includes(text)) {
+        div.innerHTML = div.innerHTML.replace(
+          text,
+          `<span class="rabbit-lyrics-searched">${text}</span>`
+        );
       }
       if (option === "highlight") {
-          div.classList.add('rabbit-lyrics-highlighted');
+        const selected = window.getSelection();
+        console.log(selected);
       }
+    }
+  }
+
+  function revertSearch() {
+    const searched = document.getElementsByClassName("rabbit-lyrics-searched");
+    while (searched.length) {
+      const div = searched.item(0);
+      div.classList.remove("rabbit-lyrics-searched");
+    }
+  }
+
+  function revertHighlighted() {
+    const highlighted = document.getElementsByClassName(
+      "rabbit-lyrics-highlighted"
+    );
+
+    while (highlighted.length) {
+      const div = highlighted.item(0);
+      div.classList.remove("rabbit-lyrics-highlighted");
     }
   }
 }
